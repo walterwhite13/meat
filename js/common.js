f47fd8e59632a1a22888effcea2a2185efac1eab
walterwhite13 <brbad13@gmail.com>
2017-02-24 14:06:59 +0300

    asd
    ads

--- js/common.js
+++ js/common.js
@@ -11,4 +11,46 @@ $(document).ready(function() {
 		nextArrow: '<img src="img/next.png" class="arrow arrow2" alt="" />'
 	});
 
+	$('.award').slick({
+		infinite: true,
+		slidesToShow: 10,
+		slidesToScroll: 3,
+		prevArrow: '<div class="arr-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',
+		nextArrow: '<div class="arr-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',
+		responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 8
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 6
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+	});
+
+	$('.hamburger').click(function(){
+		$('.menu-2').toggle();
+		$('.hamburger i').toggleClass("fa-bars fa-times");
+	});
+
+	$(window).resize(function(){
+		if (window.innerWidth > 992) {
+			$('.menu-2').hide();
+			$('.hamburger i').removeClass("fa-bars").removeClass("fa-times").addClass("fa-bars");
+		}
+	});
+
+
 });
